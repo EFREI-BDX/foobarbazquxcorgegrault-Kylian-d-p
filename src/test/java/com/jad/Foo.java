dabcf0de8e8ff6a8849ab41a52545064a9f53424
Kylian Dupouy Peron <kdperon@yahoo.com>
2025-02-14 18:48:24 +0100

    update

--- src/test/java/com/jad/Foo.java
+++ src/test/java/com/jad/Foo.java
@@ -1,4 +1,52 @@
 package com.jad;
 
+import java.util.ArrayList;
+
 public class Foo {
+
+    private final ArrayList<Baz> bazs;
+    private final ArrayList<Grault> graults;
+    private Bar bar;
+    private Qux qux;
+    private Corge corge;
+
+    public Foo(Bar bar) {
+        this.bar = bar;
+        this.bazs = new ArrayList<>();
+        this.graults = new ArrayList<>();
+        this.qux = new Qux();
+    }
+
+    public Bar getBar() {
+        return this.bar;
+    }
+
+    public ArrayList<Baz> getBazs() {
+        return this.bazs;
+    }
+
+    public Qux getQux() {
+        return this.qux;
+    }
+
+    public Corge getCorge() {
+        return this.corge;
+    }
+
+    public void setCorge(Corge corge) {
+        this.corge = corge;
+        corge.setFoo(this);
+    }
+
+    public ArrayList<Grault> getGraults() {
+        return this.graults;
+    }
+
+    public void addBaz(Baz baz) {
+        this.bazs.add(baz);
+    }
+
+    public void addGrault() {
+        this.graults.add(new Grault(this));
+    }
 }
